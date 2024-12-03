5d97a8a669fed6cc06597fc7b989039104b23dc0
Mlbrands2 <maliwajohn6295@gmail.com>
2024-12-03 16:56:59 +0300

    edited

--- app/(back-office)/layout.js
+++ app/(back-office)/layout.js
@@ -4,16 +4,16 @@ import Navbar from '@/components/backstore/Navbar';
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col md:flex-row h-screen">
+    <div className="flex gap-4 flex-col md:flex-row h-screen bg-gradient-to-r from-blue-100 to-indigo-100">
       {/* Sidebar */}
-      <Sidebar/>
+      <Sidebar />
       
       <div className="flex-1 flex flex-col">
         {/* Navbar */}
-        <Navbar/>
+        <Navbar />
         
         {/* Main Content */}
-        <main className="p-4 overflow-auto">
+        <main className="p-8 bg-slate-400 text-white overflow-auto rounded-lg shadow-2xl min-h-screen mt-16 md:mt-20">
           {children}
         </main>
       </div>
