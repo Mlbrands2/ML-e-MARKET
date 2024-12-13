4b606de639d9b71f2e2f9c9021b6204ac14f2552
Mlbrands2 <maliwajohn6295@gmail.com>
2024-12-13 01:37:11 -0500

    edit

---
+++ app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NewCategory(){
+  return (
+    <div>
+       <h2>New categories</h2>
+    </div>
+  );
+};
---
+++ app/(back-office)/dashboard/(catalogue)/categories/update/[id]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function UpdateCategory(){
+  return (
+    <div>
+      <h2>Update category</h2>
+    </div>
+  );
+};
