4b9259ae7aec4ccb24939dc9c5d11b4f19b3b508
Mlbrands2 <maliwajohn6295@gmail.com>
2024-12-13 01:57:52 -0500

    edited

--- app/(back-office)/dashboard/(catalogue)/categories/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/categories/page.jsx
@@ -1,9 +1,18 @@
+import Heading from '@/components/backstore/Heading';
+import { Plus } from 'lucide-react';
+import Link from 'next/link';
 import React from 'react';
 
 export default function page(){
   return (
     <div>
       {/* header */} 
+      <div className="flex">
+        <Heading title="Categories"/>
+        <Link href='/dashboard/categories/new'><Plus/>
+        <span>Add Categories</span>
+        </Link>
+      </div>
       {/* table */}
       <h2>categories</h2>
     </div>
